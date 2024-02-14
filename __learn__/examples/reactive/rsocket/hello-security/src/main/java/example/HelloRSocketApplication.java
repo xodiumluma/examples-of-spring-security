a4349d58c8834418d6d08151d4382ebaaf82ec87
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2024-02-15 00:25:48 +1100

    [learn] set up main demo class

---
+++ __learn__/examples/reactive/rsocket/hello-security/src/main/java/example/HelloRSocketApplication.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RSocket Security minimalistic demo
+ */
+@SpringBootApplication
+public class HelloRSocketApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HelloRSocketApplication.class, args);
+  }
+}
