8d4986a83f8c9ee5e1beacc23a67cfba5f28aa5a
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2024-05-25 19:43:41 +1000

    [learn] start test

---
+++ __learn__/examples/reactive/rsocket/hello-security/src/integTest/java/example/HelloRSocketApplicationTests.java
@@ -0,0 +1,14 @@
+package example;
+
+import io.rsocket.metadata.WellKnownMimeType;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rsocket.server.LocalRSocketServerPort;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+public class HelloRSocketApplicationTests {
+  
+}
